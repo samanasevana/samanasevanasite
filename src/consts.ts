ef66c80fb77e47a2347a93cd4c1df7602fd574c6
Jordan Rein <jordan@basilica.digital>
2025-03-03 20:01:03 -0800

    Update site url

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://bhantegunaviro.netlify.app";
+export const SITE_URL = "https://www.samanasevana.org";
 export const SITE_TITLE = "Samaṇa Sevanā";
 export const SITE_DESCRIPTION =
   "A community website dedicated to following and supporting Āyasmā Guṇavīro. Organized and maintained by lay supporters, it provides news, updates, and guidance for those who wish to stay connected with his journey and teachings.";
